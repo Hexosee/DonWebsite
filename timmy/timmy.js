425d1c78c843469a417f1d31c351d0d4dbc7e56b
ms-kaylaa <108439098+ms-kaylaa@users.noreply.github.com>
2026-03-29 21:39:19 -0400

    timmy into subfolder

--- timmy/timmy.js
+++ timmy/timmy.js
@@ -1,11 +1,11 @@
 const timmy = document.getElementById("timmy")
 const timmytalk = document.getElementById("timmytx")
-const breath = new Audio("sounds/timmy breath.wav")
+const breath = new Audio("sound/timmy breath.wav")
 console.log(timmy)
 
 timmy.onclick = function() {
     timmytalk.remove()
-    timmy.src = "/Images/timmybreath.png"
+    timmy.src = "img/timmybreath.png"
     breath.play()
 
     setTimeout(function() {
