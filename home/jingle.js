fccf7cea5bd0b30b72c9211687f1b97888ac2670
ms-kaylaa <108439098+ms-kaylaa@users.noreply.github.com>
2026-04-11 10:55:58 -0400

    made jingle not always play

--- home/jingle.js
+++ home/jingle.js
@@ -1,8 +1,11 @@
-const audio = document.getElementById("jingle");
+if(localStorage.getItem('playjingle') == 'true') {
+    localStorage.setItem('playjingle', 'false')
 
-window.addEventListener("load", () => {
-  audio.volume = 0.3; // uhhh 1 is max and then its like decimals that go by 10's and 100's places
-  audio.play().catch(() => {
-    console.log("cant autoplay dumbfuck");
-  });
-});
+    const audio = document.createElement("audio")
+    audio.src = "sound/darcys-jingle.mp3"
+
+    audio.volume = 0.3; // uhhh 1 is max and then its like decimals that go by 10's and 100's places
+    audio.play().catch(() => {
+        console.log("cant autoplay dumbfuck");
+    });
+}
