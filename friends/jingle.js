1b9f566c44064f0be703f8fe1a942b61e659756c
DonaldAni <donaldani1010@gmail.com>
2026-04-12 16:50:54 -0400

    dni page
    AND SPANKED ME!!!!!

---
+++ friends/jingle.js
@@ -0,0 +1,11 @@
+if(localStorage.getItem('playjingle') == 'true') {
+    localStorage.setItem('playjingle', 'false')
+
+    const audio = document.createElement("audio")
+    audio.src = "sound/darcys-jingle.mp3"
+
+    audio.volume = 0.3; // uhhh 1 is max and then its like decimals that go by 10's and 100's places
+    audio.play().catch(() => {
+        console.log("cant autoplay dumbfuck");
+    });
+}
