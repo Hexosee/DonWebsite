a9f2a770dcfdcdd6d419ebdb74d80f77d4a0b53f
Kayla <108439098+ms-kaylaa@users.noreply.github.com>
2026-04-12 16:53:41 -0400

    fix audio i think
    i'm on my phone i can't really check right now

---
+++ dni/audio.js
@@ -0,0 +1,7 @@
+const audio = document.createElement("audio")
+audio.src = "sound/DNI.wav"
+
+audio.volume = 0.3; // uhhh 1 is max and then its like decimals that go by 10's and 100's places
+audio.play().catch(() => {
+    console.log("cant autoplay dumbfuck");
+});
