9ffc3a06db66dc3358faf82b8a44ec47c71dc145
DonaldAni <donaldani1010@gmail.com>
2026-06-12 19:22:39 -0400

    chat names

--- chatroom/js/chatroom/specialnames.js
+++ chatroom/js/chatroom/specialnames.js
@@ -31,7 +31,8 @@ export const namemap = {
     "donaldani": new SpecialName('#ff7700', 'donaldrecieve'), // this is -1
     "ms_kaylaa": new SpecialName('#ff72c0', 'kaylarecieve'), // this is -2
     "ukubabe": new SpecialName('#575799', 'ukurecieve'), // this is -3
-    "paint": new SpecialName('#1c2dc5', 'paintrecieve') // etc
+    "paint": new SpecialName('#1c2dc5', 'paintrecieve'), // etc
+    "hexose": new SpecialName('#9817d4', 'hexoserecieve') // etc
 }
 
 export function checkifspecial(name) {
