55e8b2b3601f8852b63b25e927b24f3abdce7cb9
ms-kaylaa <108439098+ms-kaylaa@users.noreply.github.com>
2026-06-12 16:35:51 -0400

    v1

---
+++ chatroom/js/chatroom/specialnames.js
@@ -0,0 +1,42 @@
+let spidx = 0
+class SpecialName {
+    constructor(color, notifsound) {
+        this.icon = --spidx
+        this.color = color
+
+        if(notifsound !== undefined) {
+            this.notifsound = document.createElement("audio")
+                this.notifsound.src = `sound/special/${notifsound}.mp3`
+        }
+    }
+
+    getcss() {
+        return `
+            color: ${this.color};
+            border-bottom: 1px dashed ${this.color};
+        `
+    }
+
+    getnotifsound() {
+        if(this.notifsound) {
+            return this.notifsound
+        }
+
+        return RECIEVE_SOUND
+    }
+}
+
+// you need to add in order of the icons, descending. e.g.:
+export const namemap = {
+    "donaldani": new SpecialName('#ff7700', 'donaldrecieve'), // this is -1
+    "ms_kaylaa": new SpecialName('#ff72c0', 'kaylarecieve'), // this is -2
+    "ukubabe": new SpecialName('#575799', 'ukurecieve'), // this is -3
+    "paint": new SpecialName('#1c2dc5', 'paintrecieve') // etc
+}
+
+export function checkifspecial(name) {
+    if(name.toLowerCase() in namemap) {
+        return namemap[name.toLowerCase()]
+    }
+    return false
+}
